126771c88f1e13aaf2be0c86adbf0942a971cbfc
Simon Grätzer <simon@graetzer.org>
2013-10-27 01:11:23 +0200

    Usability changes

--- app/models/models.go
+++ app/models/models.go
@@ -57,11 +57,11 @@ func (p *Post) SetUpdatedTime(t time.Time) {
 	p.Updated = t.Unix()
 }
 
-// Just the first 150 characters (+"..."), may not be very good
+// Just the first 200 characters (+"..."), may not be very good
 // considering that this is supposed to contain valid markdown
 func (p Post) Summary() string {
-	if len(p.Body) > 150 {
-		return p.Body[0:150] + "..."
+	if len(p.Body) > 200 {
+		return p.Body[0:200] + "..."
 	}
 	return p.Body
 }
